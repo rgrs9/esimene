f33409314388851e3f8388a5ebd32b5c712eac4d
rgrs9 <108997831+rgrs9@users.noreply.github.com>
2024-02-27 19:40:21 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./luuletus.css"
+import Luuletus from './Luuletus';
+
+function App() {
+  return (
+    <div className="konteiner">
+      <h1>Luuletus</h1>
+      <Luuletus/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Luuletus.js
@@ -0,0 +1,12 @@
+function Luuletus() {
+    return (
+        <div>
+
+            <p>Kes hommikuti külas käib,</p>
+            <p>see asjatult ei longi</p>
+            <p>sest selleks hommik ongi!</p>
+        </div>
+    )
+}
+
+export default Luuletus;
